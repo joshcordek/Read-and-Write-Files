31398012368e7455e14599211e7a139123efde4a
joshcordek <josh_cordek1@baylor.edu>
2024-06-10 15:43:08 -0700

    Update employee_bonus.py

--- employee_bonus.py
+++ employee_bonus.py
@@ -1,17 +1,12 @@
-import pandas as pd
+import csv
 
-# Load the file
 file_path = 'employee_data.csv'
-employee_df = pd.read_csv(file_path)
 
-# Display data
-for index, row in employee_df.iterrows():
-    print('Employee ID:', row['ID'])
-    print('Name:', row['Name'])
-    print('Age:', row['Age'])
-    print('Salary:', row['Salary'])
-    print('Hours:', row['HoursWorked'])
-    print('Productivity:', row['Productivity'])
-    print('Team:', row['Team'])
-    print('Bonus:', row['Bonus'])
-    print("")
+with open(file_path, 'r') as file:
+    employee_data_reader = csv.reader(file)
+    headers = next(employee_data_reader)
+    
+    for row in employee_data_reader:
+        for header, value in zip(headers, row):
+            print(f"{header}: {value}")
+        print("")
